fa3184c1de4bc56b09c810e2b89d95ed2bc454ac
Antonio Santana <104534239+MiguelScofielD@users.noreply.github.com>
2024-08-30 17:26:24 -0400

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,53 @@
+import subprocess
+import os
+from datetime import datetime
+
+# Configurações dos bancos de dados e backup
+DATABASES = [
+    {"name": "rev2_0413_3_so", "user": "postgres", "host": "localhost", "port": "5432"},
+    {"name": "rev2_0518_2_so", "user": "postgres", "host": "localhost", "port": "5432"},
+    # Adicione mais bancos de dados conforme necessário
+]
+
+BACKUP_DIR = "C:/Users/antdp/Downloads/backup"
+
+def backup_database(db_name, user, host, port):
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    backup_file = os.path.join(BACKUP_DIR, f"{db_name}_{timestamp}.sql")
+
+    # Atualize o caminho para pg_dump
+    pg_dump_path = "C:/Program Files/PostgreSQL/15/bin/pg_dump.exe"
+
+    # Construa o comando
+    cmd = [
+        pg_dump_path,
+        "-U", user,
+        "-h", host,
+        "-p", port,
+        "-F", "c",
+        "-f", backup_file,
+        db_name
+    ]
+    
+    # Define a variável de ambiente para a senha
+    env = os.environ.copy()
+    env["PGPASSWORD"] = "postgres"
+    
+    # Exibir o comando para debugging
+    print(f"Executando comando: {' '.join(cmd)}")
+    
+    # Execute o comando e capture a saída de erro
+    result = subprocess.run(cmd, env=env, text=True, capture_output=True)
+    
+    # Exibir o código de retorno e mensagem de erro para debugging
+    if result.returncode == 0:
+        print(f"Backup do banco {db_name} concluído com sucesso.")
+    else:
+        print(f"Erro ao fazer backup do banco {db_name}: Código de erro {result.returncode}")
+        print(f"Saída de erro: {result.stderr}")
+
+# Cria o diretório de backup se não existir
+os.makedirs(BACKUP_DIR, exist_ok=True)
+
+for db in DATABASES:
+    backup_database(db["name"], db["user"], db["host"], db["port"])
